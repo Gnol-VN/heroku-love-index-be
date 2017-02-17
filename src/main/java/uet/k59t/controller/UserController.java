2bdc01b3b5e069ccba17171e412e32f1594a1f06
Long <zzsushiboyzz@gmail.com>
2017-02-17 09:58:26 +0700

    post with body

--- src/main/java/uet/k59t/controller/UserController.java
+++ src/main/java/uet/k59t/controller/UserController.java
@@ -1,10 +1,19 @@
 package uet.k59t.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import uet.k59t.controller.dto.UserDTO;
 import uet.k59t.model.User;
 import uet.k59t.service.UserService;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by Long on 11/21/2016.
@@ -17,7 +26,21 @@ public class UserController {
     //create User
     @RequestMapping(value = "user", method = RequestMethod.POST)
     public UserDTO createUser(@RequestBody UserDTO userDTO){
+        String casUrlPrefix = "http://login-test.his.com/tcgroup-sso-web";
+        String username = "15372712873";
+        String password = "123456";
+
+        // GET TGT
+        RestTemplate rest = new RestTemplate();
+        rest.setMessageConverters(Arrays.asList(new StringHttpMessageConverter(), new FormHttpMessageConverter()));
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.set("username", username);
+        params.set("password", password);
+        URI tgtUrl = rest.postForLocation(casUrlPrefix + "/v1/tickets", params, Collections.emptyMap());
+        System.out.println("[" + tgtUrl + "]");
+
         return userService.createUser(userDTO);
+
     }
 
     //find User by id
--- src/main/java/uet/k59t/service/HelloService.java
+++ src/main/java/uet/k59t/service/HelloService.java
@@ -4,15 +4,47 @@ package uet.k59t.service;
  * Created by Long on 11/20/2016.
  */
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import uet.k59t.controller.HelloController;
+
+import java.util.Arrays;
 
 @Service
 public class HelloService {
 
     public String sayHello(String s){
+        //Post with body
+        RestTemplate rest = new RestTemplate();
+        rest.setMessageConverters(Arrays.asList(new StringHttpMessageConverter(), new FormHttpMessageConverter()));
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.set("rapide_annee[0]", "96");
+        params.set("rapide_annee[1]", "96");
+        params.set("rapide_jour[0]", "14");
+        params.set("rapide_jour[1]", "24");
+        params.set("rapide_mois[0]", "10");
+        params.set("rapide_mois[1]", "2");
+        params.set("rapide_prenom[0]", "long");
+        params.set("rapide_prenom[1]", "e");
+        params.set("partenaire", "9999");
+        params.set("lang", "en");
+        ResponseEntity<String> st = rest.postForEntity("http://www.astrotheme.fr/partenaires/indice_rapide.php", params, String.class);
+        String html = st.getBody();
+
+        Document document = Jsoup.parse(html);
+        Elements divs = document.select("div");
+        String score = divs.get(2).ownText();
+        String comment = document.select("p").get(0).ownText();
         return "Hello, " +s;
     }
 }
